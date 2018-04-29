70633ba507b980f562e111bb8658e7def8c27a3d
Yiteng Guo <yg229@cornell.edu>
2018-04-29 16:49:25 -0400

    implement render obj and collider

--- data_parser.ts
+++ data_parser.ts
@@ -6,17 +6,29 @@ import { mat4, vec3 } from 'gl-matrix';
 declare function require(name: string): any;
 
 interface Light {
-    color: Array<number>;
-    position: Array<number>;
+    color: Array<number>; // length = 3
+    position: Array<number>; // length = 3
     intensity: number;
 }
 
 interface Transform {
-    translation: Array<number>;
+    translation: Array<number>; // length = 4
     rotationX: number;
     rotationY: number;
     rotationZ: number;
-    scale: Array<number>;
+    scale: Array<number>; // length = 4
+}
+
+interface RenderObject {
+    mesh: string;
+    texture: string;
+    shader: string;
+}
+
+interface Collider {
+    name: string;
+    param: Array<number>; // length = 2
+    transform: Transform;
 }
 
 const format_num = new Intl.NumberFormat('en-US', {minimumFractionDigits: 1}).format;
@@ -44,6 +56,14 @@ function compile_transform(trans: Transform) {
     const rZ = `mat4.rotateZ(mat4(), mat4(), ${format_num(trans.rotationZ)})`;
     const s = `mat4.scale(mat4(), mat4(), ${parse_vec(trans.scale)})`;
     return `(${t}, ${rX}, ${rY}, ${rZ}, ${s})`;
+}
+
+function compile_render_object(obj: RenderObject) {
+    return `(load_obj(${obj.mesh}), load_texture(${obj.texture}), "${obj.shader}")`;
+}
+
+function compile_collider(c: Collider) {
+    return `("${c.name}", ${parse_vec(c.param)}, ${compile_transform(c.transform)})`;
 }
 
 export function compile(filename: string) {
