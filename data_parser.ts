03aea9f9a1871504bf72fd5598a90123a02b558c
Yiteng Guo <yg229@cornell.edu>
2018-04-29 18:44:35 -0400

    implement data parser

--- data_parser.ts
+++ data_parser.ts
@@ -31,9 +31,65 @@ interface Collider {
     transform: Transform;
 }
 
-const format_num = new Intl.NumberFormat('en-US', {minimumFractionDigits: 1}).format;
+interface Trigger {
+    name: string;
+    filename: string;
+    condition: string;
+    action: string;
+}
 
-function parse_vec(v: Array<number>) {
+interface GameObject {
+    name: string;
+    position: Array<number>; // length = 3
+    local_transform: Transform;
+    render_obj: RenderObject;
+    collider: Collider;
+    lights: Array<Light>;
+    triggers: Array<Trigger>;
+}
+
+interface Room {
+    name: string;
+    lights: Array<Light>;
+    objects: Array<GameObject>;
+    size: Array<number>; // length = 3
+    texture: string;
+    start_position: Array<number>; // length = 2
+    start_direction: Array<number>; // length = 3
+}
+
+type Inventory = string;
+
+interface Player {
+    name: string;
+    collider: Collider;
+    inventories: Array<Inventory>;
+    height: number;
+    render_obj: RenderObject;
+    transform: Transform;
+}
+
+interface Game {
+    starting_room: string;
+    rooms: Array<Room>;
+    player: Player;
+}
+
+const newline = "\n";
+
+const format_num = new Intl.NumberFormat('en-US', { minimumFractionDigits: 1 }).format;
+
+const triggers_map = new Map<string, Array<Trigger>>();
+
+function create_tuple(l: Array<string>) {
+    return `(${l.join(', ')})`;
+}
+
+function create_newline_tuple(l: Array<string>) {
+    return `(${newline}${l.join(', ' + newline)}${newline})`;
+}
+
+function compile_vec(v: Array<number>) {
     if (v.length === 4) {
         return `vec4(${v.map(format_num).join(', ')})`;
     } else if (v.length === 3) {
@@ -46,16 +102,16 @@ function parse_vec(v: Array<number>) {
 }
 
 function compile_light(light: Light) {
-    return `(${parse_vec(light.color)}, ${parse_vec(light.position)}, ${format_num(light.intensity)})`;
+    return `(${compile_vec(light.color)}, ${compile_vec(light.position)}, ${format_num(light.intensity)})`;
 }
 
 function compile_transform(trans: Transform) {
-    const t = `mat4.translate(mat4(), mat4(), ${parse_vec(trans.translation)})`;
+    const t = `mat4.translate(mat4(), mat4(), ${compile_vec(trans.translation)})`;
     const rX = `mat4.rotateX(mat4(), mat4(), ${format_num(trans.rotationX)})`;
     const rY = `mat4.rotateY(mat4(), mat4(), ${format_num(trans.rotationY)})`;
     const rZ = `mat4.rotateZ(mat4(), mat4(), ${format_num(trans.rotationZ)})`;
-    const s = `mat4.scale(mat4(), mat4(), ${parse_vec(trans.scale)})`;
-    return `(${t}, ${rX}, ${rY}, ${rZ}, ${s})`;
+    const s = `mat4.scale(mat4(), mat4(), ${compile_vec(trans.scale)})`;
+    return create_newline_tuple([t, rX, rY, rZ, s]);
 }
 
 function compile_render_object(obj: RenderObject) {
@@ -63,13 +119,51 @@ function compile_render_object(obj: RenderObject) {
 }
 
 function compile_collider(c: Collider) {
-    return `("${c.name}", ${parse_vec(c.param)}, ${compile_transform(c.transform)})`;
+    return create_tuple([c.name, compile_vec(c.param), compile_transform(c.transform)]);
+}
+
+function compile_object(o: GameObject) {
+    const name = o.name;
+    const position = compile_vec(o.position);
+    const l_trans = compile_transform(o.local_transform);
+    const r_obj = compile_render_object(o.render_obj);
+    const collider = compile_collider(o.collider);
+    const lights = `array{Light}${create_newline_tuple(o.lights.map(compile_light))}`;
+    const triggers = `__triggers_${o.name}`;
+    triggers_map.set(triggers, o.triggers);
+    const l = [name, position, l_trans, r_obj, collider, lights, triggers];
+    return create_newline_tuple(l);
+}
+
+function compile_room(r: Room) {
+    const name = r.name;
+    const lights = `array{Light}${create_newline_tuple(r.lights.map(compile_light))}`;
+    const objects = `array{Object}${create_newline_tuple(r.objects.map(compile_object))}`;
+    const size = compile_vec(r.size);
+    const texture = `load_texture(${r.texture})`;
+    const sp = compile_vec(r.start_position);
+    const sd = compile_vec(r.start_direction);
+    return create_newline_tuple([name, lights, objects, size, texture, sp, sd]);
+}
+
+function compile_player(p: Player) {
+    const name = p.name;
+    const collider = compile_collider(p.collider);
+    const invs = `array{Inventory}${create_tuple(p.inventories)}`;
+    const height = format_num(p.height);
+    const ro = compile_render_object(p.render_obj);
+    const trans = compile_transform(p.transform);
+    return create_newline_tuple([name, collider, invs, height, ro, trans]);
+}
+
+function compile_game(g: Game) {
+    return create_newline_tuple([g.starting_room, `array{Room}${create_newline_tuple(g.rooms.map(compile_room))}`, compile_player(g.player)]);
 }
 
 export function compile(filename: string) {
     let data = require("./data/" + filename);
-    const start_room: String = data.start_room;
-    return start_room;
+    const d: Transform = data;
+    return d;
 }
 
 console.log(123);
